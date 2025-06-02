630daa32c0362c65226e50d94041ce2459d530b4
Christian Köberl <christian.koeberl@porscheinformatik.com>
2025-06-02 22:53:05 +0200

    add dealer info

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  // <- important for server-side rendering
-  output: "server",
+  output: "server", // <- important for server-side rendering
 
   vite: {
     plugins: [tailwindcss()],
