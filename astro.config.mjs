bd81d2274c0a95c5475cb9c1b5490ecfb0b65cbd
Christian Köberl <christian.koeberl@porscheinformatik.com>
2025-06-02 22:04:59 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // <- important for server-side rendering
+  // <- important for server-side rendering
+  output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
